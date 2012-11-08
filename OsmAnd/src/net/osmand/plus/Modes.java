bc80b55bb67053e58993046a4443085521de6f2b
Marcelo Martins <martins@cs.brown.edu>
2012-11-07 21:21:54 -0500

    Added modes declaration

---
+++ OsmAnd/src/net/osmand/plus/Modes.java
@@ -0,0 +1,15 @@
+package net.osmand.plus;
+
+
+/**
+ * @author Marcelo Martins <martins@cs.brown.edu>
+ *
+ * Application-mode labels
+ */
+public class Modes {
+
+		public static final String FULL_FEATURES = "Full Features";
+		public static final String LIGHT_SCREEN = "Light Screen";
+		public static final String DARK_SCREEN = "Dark Screen";
+		public static final String AUDIO_ONLY = "Audio Only";
+}
